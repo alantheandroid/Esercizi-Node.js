d17d77e1304de791efbdd20df2f0e9cc62f9b071
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-14 15:53:39 +0200

    📦 NEW:  add API route to retrieve resources

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  verbose: false,
+  verbose: true,
+  clearMocks: true,
+  setupFilesAfterEnv: ["./16_retrieve_all_resources/lib/prisma/client.mock.ts"],
 };
