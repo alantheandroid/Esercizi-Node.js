915e1cd77be64e3c616f0b485b70f7bcc866bb9f
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-19 15:51:33 +0200

    👌 IMPROVE: implement toString method on Shout class

--- design-patterns/decorator/decorator.js
+++ design-patterns/decorator/decorator.js
@@ -12,6 +12,10 @@ class Shout {
   constructor(text) {
     this.text = text;
   }
+
+  toString() {
+    return this.text.toString().toUpperCase();
+  }
 }
 
 console.log(new Text("Hello, I'm talking").toString());
