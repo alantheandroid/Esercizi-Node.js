20763f742c373b46be5693197e7fb06cb9cb3fe5
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-19 13:05:45 +0200

    📦 NEW: add exercise 25

---
+++ design-patterns/decorator/decorator.js
@@ -0,0 +1,19 @@
+class Text {
+  constructor(text) {
+    this.string = text;
+  }
+
+  toString() {
+    return this.string;
+  }
+}
+
+class Shout {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+console.log(new Text("Hello, I'm talking").toString());
+
+console.log(new Shout(new Text("Hello, I'm shouting!")).toString());
